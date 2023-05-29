563403de1983f2441f7dc34f707606f24a633591
DESKTOP-08QMB4I\bangsun <zx@bsfit.com.cn>
2023-05-29 18:51:41 +0800

    前三章部分代码

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { AppModule } from './app.module';
+import * as path from 'path';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(path.join(__dirname, '..', 'public'), {
+    prefix: '/static',
+  });
+  await app.listen(3000);
+}
+bootstrap();
---
+++ src/person/person.controller.ts
@@ -0,0 +1,43 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+} from '@nestjs/common';
+import { PersonService } from './person.service';
+import { CreatePersonDto } from './dto/create-person.dto';
+import { UpdatePersonDto } from './dto/update-person.dto';
+
+@Controller('api/person')
+export class PersonController {
+  constructor(private readonly personService: PersonService) {}
+
+  @Post()
+  create(@Body() createPersonDto: CreatePersonDto) {
+    return `received: ${JSON.stringify(createPersonDto)}`;
+  }
+
+  @Get('find')
+  query(@Query('name') name: string, @Query('age') age: number) {
+    return `received: name=${name},age=${age}`;
+  }
+
+  @Get(':id')
+  urlParam(@Param('id') id: string) {
+    return `received: id=${id}`;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updatePersonDto: UpdatePersonDto) {
+    return this.personService.update(+id, updatePersonDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.personService.remove(+id);
+  }
+}
